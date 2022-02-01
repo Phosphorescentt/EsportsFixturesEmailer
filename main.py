9d3f784660df18a5de0227c42c19f30f387038db
Joshua Mankelow <joshua@mankelow.org>
2022-02-01 23:50:29 +0000

    Refactored email composition so that there's more data in there and it's formatted slightly better

--- main.py
+++ main.py
@@ -13,14 +13,40 @@ ps = PandaScore.PandaScoreAPIClient(SECRETS.PS_API_KEY)
 
 tournaments = ps.get_running_tournaments()
 
-tournament_slugs = [t["slug"] for t in tournaments]
+# Next actions for me 
+# 1) Get rid of all the garbage tournaments that I don't care about
+# ==> Filtering by game
+# ==> Filtering by region
+# 2) Show results for finished matches
+# 3) Improve formatting (include images, actually use formatting stuff)
 
-data_content = "<br>".join(tournament_slugs)
+html_content = ""
+for tournament in tournaments:
+    output_string = ""
+
+    league = tournament["league"]
+    serie = tournament["serie"]
+    tournament = tournament
+    matches = tournament["matches"]
+
+    output_string += "<h1>" + league["name"] + "</h1><br>"
+
+    try:
+        output_string += "<h2>" + serie["name"] + "</h2><br>"
+    except:
+        pass
+
+    output_string += "<h3>" + tournament["name"] + "</h3><br>"
+
+    for match in matches:
+        output_string += match["name"] + "<br>"
+
+    html_content += output_string + "<hr>"
 
 from_email = Email(CONFIG.FROM_EMAIL)
 to_email = To(CONFIG.TO_EMAIL)
 subject = CONFIG.EMAIL_SUBJECT
-content = HtmlContent(data_content)
+content = HtmlContent(html_content)
 mail = Mail(from_email, to_email, subject, content)
 
 response = sg.send(message=mail)
